f475a35acdb2be5f7fb1b9d84c9600f407e26e98
German Martinez <mmgerman89@gmail.com>
2014-07-08 16:11:58 -0300

    Iteration D2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,13 @@ class ApplicationController < ActionController::Base
   def date_loaded
     Time.now
   end
+  
+  private
+    def current_cart
+      Cart.find(session[:cart_id])
+    rescue ActiveRecord::RecordNotFound
+      cart = Cart.create
+      session[:cart_id] = cart.id
+      cart
+    end
 end
