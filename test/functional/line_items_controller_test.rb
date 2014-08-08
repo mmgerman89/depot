b7ddeea8cb23f7028eec941f379a97b30b3e7512
German Martinez <mmgerman89@gmail.com>
2014-08-08 16:08:56 -0300

    Iteration F4

--- test/functional/line_items_controller_test.rb
+++ test/functional/line_items_controller_test.rb
@@ -21,7 +21,7 @@ class LineItemsControllerTest < ActionController::TestCase
       post :create, :product_id => products(:ruby).id#:line_item => @line_item.attributes
     end
 
-    assert_redirected_to cart_path(assigns(:line_item).cart)
+    assert_redirected_to store_path#cart_path(assigns(:line_item).cart)
   end
 
   test "should show line_item" do
@@ -47,5 +47,15 @@ class LineItemsControllerTest < ActionController::TestCase
     assert_redirected_to Cart.find(session[:cart_id])
   end
   
+  test "should create line_item via ajax" do
+    assert_difference('LineItem.count') do
+      xhr :post, :create, product_id: products(:ruby).id
+    end
+    
+    assert_response :success
+    assert_select_rjs :replace_html, 'cart' do
+      assert_select 'tr#current_item td', /Programming Ruby 1.9/
+    end
+  end
   
 end
